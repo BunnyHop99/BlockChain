fa1792c14583a07e91062d34fa6193142a70b859
Andres Sanchez <Bunny_HoPp99@hotmail.com>
2021-09-15 11:59:26 -0700

    blockchain

---
+++ index.js
@@ -0,0 +1,9 @@
+import PKG from './package.json';
+import Block from './src/blockchain/block';
+
+const {name, version} = PKG;
+
+console.log(`${name} ${version}`);
+
+const block = new Block(Date.now(), 'pr3v10s-h4sh', 'h4sh', 'd4t4');
+console.log(block.toString());
